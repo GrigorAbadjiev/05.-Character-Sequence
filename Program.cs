84061b4508dff0c038033a2a9172dc012ff4992e
User <User@DESKTOP-ESP2JT1>
2022-11-21 11:29:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            for (int index = 0; index < word.Length; index++)
+            {
+                Console.WriteLine(word[index]);
+            }
+        }
+    }
+}
